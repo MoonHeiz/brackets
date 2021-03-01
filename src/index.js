a4b83a13223a610e5476624fcb8b9418cb0c8c36
Andrey Morozov <MoonHeiz@yandex.ru>
2021-03-01 17:18:39 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    for(let symbol of str){
+        if(symbol === stack[stack.length - 1]){
+            stack.pop();
+            continue;
+        }
+
+        const found = bracketsConfig.find(subarray => symbol === subarray[0]);
+
+        if(found){
+            stack.push(found[1]);
+        }
+        else return false;
+    }
+  
+    return stack.length === 0 ? true : false;   
 }
